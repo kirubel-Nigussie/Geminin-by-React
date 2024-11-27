2bad8f803d0f817791ffc8a8b0f0aa018a727c55
kirubel-Nigussie <kiranigussie1738@gmail.com>
2024-11-27 22:22:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Gemini-by-React
---
+++ src/config/gemini.js
@@ -0,0 +1,40 @@
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+//   const apiKey = process.env.GEMINI_API_KEY;
+const apiKey = "[You're Api key]";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+   
+  });
+  
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(promt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(promt);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  }
+  
+  export default run;
